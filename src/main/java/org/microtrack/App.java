f11fe952b73ef4195c1149a648b4781adcaf37a4
Arthur <pimentabraga2@gmail.com>
2023-09-20 16:13:49 -0300

    Utilização do sdk

--- src/main/java/org/microtrack/App.java
+++ src/main/java/org/microtrack/App.java
@@ -1,13 +1,29 @@
 package org.microtrack;
 
+import org.microtrack.dto.Trace;
+import org.microtrack.service.Manager;
+import org.microtrack.service.TraceService;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.Timestamp;
 
 @SpringBootApplication
 public class App {
 
-    public static void main( String[] args )
-    {
-        Manager manager = new Manager("Microtrack", "1234567890", "you are awesome");
-        System.out.println(manager.getDataMessage());
+    public static void main(String[] args) {
+        Manager manager = new Manager(true);
+        TraceService traceService = new TraceService();
+
+        Trace trace = Trace.builder()
+                .traceId("Batata1")
+                .serviceName("api-mock-one-microtrack")
+                .timestamp(new Timestamp(System.currentTimeMillis()))
+                .checkpointName("Checkpoint1")
+                .isError(Boolean.FALSE)
+                .build();
+
+        traceService.checkpoint(manager, trace);
+
+        System.out.println(trace.getAllString());
     }
 }
